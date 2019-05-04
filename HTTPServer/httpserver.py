9ab58394902c0270652081bccc00fc11d6a9572c
lijiulin163LJL <49966974+lijiulin163LJL@users.noreply.github.com>
2019-05-04 14:48:41 +0800

    Add files via upload

--- HTTPServer/httpserver.py
+++ HTTPServer/httpserver.py
@@ -7,4 +7,12 @@ from HTTPServer.config import *
 class HTTPServer():
     def __init__(self):
         self.sockfd = socket()
-        self.sockfd.bind(ADDR)
+        self.sockfd.bind()
+
+    def bind(self):
+       self.sockfd.bind(ADDR)
+       self.sockfd.listen(5)
+
+    def start_sever(self):
+        pass
+
