6a6a3766591f4c53d48e0bc3ee5417d1ff4e93df
Yzh88 <zh-design@qq.com>
2019-05-19 11:10:39 +0800

    0.2

---
+++ config/web_config.py
@@ -0,0 +1,10 @@
+# [frame ip]
+
+frame_ip = '127.0.0.1'
+frame_port = 8080
+frame_address = (frame_ip, frame_port)
+# [debug]
+DEBUG = True
+
+# [save html pages]
+STATIC_DIR = "/home/tarena/project/web/"
--- web/webframe.py
+++ web/webframe.py
@@ -3,58 +3,60 @@ from select import select
 from socket import *
 
 from config.web_config import *
+
+
 # from web.urls import *
 
-frame_address = (frame_ip, frame_port)
 # 应用类，将功能封装在类中
 class Application(object):
     def __init__(self):
-        self.sockfd = socket()
-        self.sockfd.setsockopt(SOL_SOCKET, SO_REUSEADDR, DEBUG)
-        self.sockfd.bind(frame_address)
+        self.sock_fd = socket()
+        self.sock_fd.setsockopt(SOL_SOCKET, SO_REUSEADDR, DEBUG)
+        self.sock_fd.bind(frame_address)
 
     def start(self):
-        self.sockfd.listen(5)
+        self.sock_fd.listen(5)
         print('Listen the port:%d' % frame_port)
-        rlist = [self.sockfd]
-        wlist = []
-        xlist = []
+        r_list = [self.sock_fd]
+        w_list = []
+        x_list = []
         # select IO多路复用
         while True:
-            rs, ws, xs = select(rlist, wlist, xlist)
+            rs, ws, xs = select(r_list, w_list, x_list)
             for r in rs:
-                if r is self.sockfd:
-                    connfd, addr = r.accept()
-                    rlist.append(connfd)
+                if r is self.sock_fd:
+                    conn_fd, addr = r.accept()
+                    r_list.append(conn_fd)
                 else:
                     self.handle(r)
-                    rlist.remove(r)
+                    r_list.remove(r)
 
-    def handle(self, connfd):
-        request = connfd.recv(1024).decode()
+    def handle(self, conn_fd):
+        request = conn_fd.recv(1024).decode()
         request = json.loads(request)
 
         # request = {'method': 'GET', 'info': '/'}
         if request['method'] == 'GET':
             if request['info'] == '/' or request['info'][-5:] == '.html':
                 response = self.get_html(request['info'])
-            else:
-                response = self.get_data(request['info'])
+            # else:
+            #     response = self.get_data(request['info'])
 
         elif request['method'] == 'POST':
             pass
 
         # 将数据发送给Http_Server
         response = json.dumps(response)
-        connfd.send(response.encode())
-        connfd.close()
+        conn_fd.send(response.encode())
+        conn_fd.close()
 
         #   测试
         # print(request.decode(), 222)
         # dict = {'status':'200','data':'OK'}
-        # connfd.send(json.dumps(dict).encode())
+        # conn_fd.send(json.dumps(dict).encode())
 
-    def get_html(self, info):
+    @staticmethod
+    def get_html(info):
         if info == '/':
             filename = STATIC_DIR + '/login.html'
         else:
@@ -68,11 +70,11 @@ class Application(object):
         else:
             return {'status': '200', 'data': fd.read()}
 
-    def get_data(self, info):
-        for url, func in urls:
-            if url == info:
-                return {'status': '200', 'data': func()}
-        return {'status': '404', 'data': 'sorry,...'}
+    # def get_data(self, info):
+    #     for url, func in urls:
+    #         if url == info:
+    #             return {'status': '200', 'data': func()}
+    #     return {'status': '404', 'data': 'sorry,...'}
 
 
 if __name__ == '__main__':
